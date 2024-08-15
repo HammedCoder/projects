32fb338b4fdf18b3d6c56de68daa2e3f04b9bacf
rafiuhammed718@gmail.com <rafiuhammed718@gmail.com>
2024-08-15 10:29:14 +0100

    multiply

--- Javascript-projects/miniCalculator/script.js
+++ Javascript-projects/miniCalculator/script.js
@@ -1,44 +1,67 @@
-const sum = document.getElementById('sum')
-const minus = document.getElementById('minus')
-const multiply = document.getElementById('multiply')
-const divide = document.getElementById('divide')
-const text1= document.getElementById('txt1')
-const text2= document.getElementById('txt2')
+let button = document.querySelectorAll('button')
+const text1 = document.getElementById("txt1");
+const text2 = document.getElementById("txt2");
+for (let i = 0; i < button.length; i++) {
+   button[0].addEventListener('click', add)
+   button[1].addEventListener('click', subtract)
+   button[2].addEventListener('click', times)
+   button[3].addEventListener('click', quotient)
+}
 
 function add() {
-    let num1 = +text1.value
-    let num2 = +text2.value
-    const total = num1 + num2
-    document.querySelector('.display').textContent=total
-    text1.value = ""
-    text2.value=""
+  if (isNaN(text1.value) && isNaN(text1.value)) {
+    alert("Delete, input must be a numeric value");
+  } else {
+    let num1 = +text1.value;
+    let num2 = +text2.value;
+    const total = num1 + num2;
+    document.querySelector(".display").textContent = total;
+    text1.value = "";
+    text2.value = "";
     console.log(total);
+  }
 }
 
 function times() {
-    let num1 = +text1.value
-    let num2 = +text2.value
-    const total = num1 * num2
-    document.querySelector('.display').textContent=total
-    text1.value = ""
-    text2.value=""
+  if (isNaN(text1.value) && isNaN(text1.value)) {
+    alert("Delete, input must be a numeric value");
+  } else {
+    let num1 = +text1.value;
+    let num2 = +text2.value;
+    const total = num1 * num2;
+    document.querySelector(".display").textContent = total;
+    text1.value = "";
+    text2.value = "";
     console.log(total);
-}function subtract() {
-    let num1 = +text1.value
-    let num2 = +text2.value
-    const total = num1 - num2
-    document.querySelector('.display').textContent=total
-    text1.value = ""
-    text2.value=""
+  }
+}
+function subtract() {
+  if (isNaN(text1.value) && isNaN(text1.value)) {
+    alert("Delete, input must be a numeric value");
+  } else {
+    let num1 = +text1.value;
+    let num2 = +text2.value;
+    const total = num1 - num2;
+    document.querySelector(".display").textContent = total;
+    text1.value = "";
+    text2.value = "";
     console.log(total);
+  }
 }
 
 function quotient() {
-    let num1 = +text1.value
-    let num2 = +text2.value
-    const total = num1 / num2
-    document.querySelector('.display').textContent=total
-    text1.value = ""
-    text2.value=""
+  if (isNaN(text1.value) && isNaN(text1.value)) {
+    alert("Delete, input must be a numeric value");
+  } else if (text2.value == 0) {
+    alert("You cannot divide by zero");
+    text2.focus()
+  } else {
+    let num1 = +text1.value;
+    let num2 = +text2.value;
+    const total = num1 / num2;
+    document.querySelector(".display").textContent = total;
+    text1.value = "";
+    text2.value = "";
     console.log(total);
+  }
 }
